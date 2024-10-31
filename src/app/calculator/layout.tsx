ef95889686f7b1c9f9f9e309a75c73d5547e47ed
Md Istiak Ahmed <istinishat@gmail.com>
2024-10-31 23:49:26 +0600

    update layout

--- src/app/calculator/layout.tsx
+++ src/app/calculator/layout.tsx
@@ -16,7 +16,7 @@ export const metadata: Metadata = {
     siteName: "RatGeber",
     images: [
       {
-        url: "payment.webp",
+        url: "/payment.JPG",
         width: 1200,
         height: 630,
         alt: "hands payment",
