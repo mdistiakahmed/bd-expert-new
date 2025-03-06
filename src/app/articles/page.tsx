622a347269f298e273a75c9b14c9d39d73144385
Md Istiak Ahmed <istinishat@gmail.com>
2025-03-06 23:47:57 +0600

    remove unnecessary sections

--- src/app/articles/page.tsx
+++ src/app/articles/page.tsx
@@ -94,41 +94,6 @@ const ArticleHomePage = async () => {
               </div>
             )
           )}
-
-          <div className="grid grid-cols-1 md:grid-cols-3 gap-6 w-full max-w-5xl px-4">
-            {posts.map((article, index) => {
-              const { heroImage } = article;
-              return (
-                <Link
-                  href={`/articles/${article?.slug?.current}`}
-                  key={index}
-                  className="bg-white shadow-md rounded-lg overflow-hidden transform transition duration-500 hover:scale-105 cursor-pointer"
-                >
-                  <Image
-                    src={urlForImage(heroImage)}
-                    height={200}
-                    width={200}
-                    className="h-[200px] w-full object-cover"
-                    alt={heroImage.alt || "post"}
-                  />
-                  <div className="p-4 pb-0">
-                    <h4 className="text-lg font-medium mb-2">
-                      {article.title}
-                    </h4>
-                  </div>
-
-                  <div className="flex items-center justify-center pb-2">
-                    <Image
-                      src="/button-arrow.svg"
-                      height={20}
-                      width={80}
-                      alt="see more"
-                    />
-                  </div>
-                </Link>
-              );
-            })}
-          </div>
         </div>
       </div>
     </div>
